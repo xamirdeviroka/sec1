63dfe01a869d9f2bfd5e672752a25dcf7d5bf7ac
xamirdeviroka <145627036+xamirdeviroka@users.noreply.github.com>
2023-09-22 18:28:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const path = require("path");
+const app = express()
+
+// #############################################################################
+// Logs all request paths and method
+app.use(function (req, res, next) {
+  res.set('x-timestamp', Date.now())
+  res.set('x-powered-by', 'cyclic.sh')
+  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
+  next();
+});
+
+// #############################################################################
+// This configures static hosting for files in /public that have the extensions
+// listed in the array.
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+  index: ['index.html'],
+  maxAge: '1m',
+  redirect: false
+}
+app.use(express.static('public', options))
+
+// #############################################################################
+// Catch all handler for all other request.
+// all the unhandled requests should be redirected to hello/here
+
+
+app.use((req,res) => {
+
+  res.redirect('/hello/here/')
+})
+
+module.exports = app
